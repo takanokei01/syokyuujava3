a201ea0f3f31e5e904dcac738e50a032ba38efc3
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-13 11:57:08 +0900

    test as

---
+++ src/start.java
@@ -0,0 +1,14 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MyFrame frame1=new RectFrame();
+	      frame1.setLocation(200,300);
+	      
+	      
+	      MyFrame frame2=new RectFrame();
+	      frame2.setLocation(300,300);
+	}
+
+}
