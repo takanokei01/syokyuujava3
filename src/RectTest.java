401c3864f9be953b5967082bb52cbeecf36e99d2
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-14 10:46:22 +0900

    java 3.1

---
+++ src/RectTest.java
@@ -0,0 +1,9 @@
+
+public class RectTest extends MyFrame {
+
+    public void run() {
+    	fillRect(30,50,200,100);
+    	fillRect(130,50,200,100);
+    	fillRect(30,1000,150,100);
+    }
+}
--- src/start.java
+++ src/start.java
@@ -3,12 +3,8 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		MyFrame frame1=new RectFrame();
-	      frame1.setLocation(200,300);
-	      
-	      
-	      MyFrame frame2=new RectFrame();
-	      frame2.setLocation(300,300);
+		System.out.println("プログラムを実行します。");
+		MyFrame frame1 = new RectTest();
 	}
 
 }
