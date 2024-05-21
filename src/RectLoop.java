ad246ef2f42a66713a1934a024e162154df9b21e
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-21 10:30:27 +0900

    4.13

--- src/RectLoop.java
+++ src/RectLoop.java
@@ -2,10 +2,11 @@
 public class RectLoop extends MyFrame {
 	public void run() {
 		setColor(0, 128, 0);
-		for(int x = 30 ; x <= 330 ; x +=120) {
+		int x = 30;
+		for(int i = 0 ; i < 15 ; i++) {
 			System.out.println(x);
 		fillRect(x, 80, 10, 100);
-		
+		x +=20;
 		}
 	}
 
