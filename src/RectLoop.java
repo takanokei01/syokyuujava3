9f4e64f5a82a241eacdfcc13bdf6b5a491a8af1b
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-21 11:34:57 +0900

    追加課題その１

--- src/RectLoop.java
+++ src/RectLoop.java
@@ -1,16 +1,15 @@
 
 public class RectLoop extends MyFrame {
 	public void run() {
-		int x = 10;
-		setColor(0, 0, 0);
-		while(x <= 190) {
-			fillRect(x, 60, 10, 100);
-			setColor(x, x, x);
-			x +=20;
-		}
-			
-		
-		
+	 int x = 5;
+	 int y = 5;
+	
+	 while(x<= 100)
+	 {   
+		 fillRect(y, y , x, x);
+		 y = y + x;
+		 x = x + 10;
+      }
 	}
 
 }
