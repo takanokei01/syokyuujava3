cbc5e5e8d5a89fb7996d832b4c6adae23af86bbd
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-21 10:03:31 +0900

    rennsyuu 3.1

---
+++ src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame {
+	public void run() {
+		fillRect(92, 100, 30, 15);
+		fillRect(80, 110, 55, 15);
+		fillOval(85, 124, 13,13);
+		fillOval(115, 124, 13,13);
+	}
+
+}
