5ab9e438bb3f84b422bdfc8241a2044616a8fbbf
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-27 11:55:31 +0900

    omake1

--- src/Animation.java
+++ src/Animation.java
@@ -4,29 +4,40 @@ public class Animation extends MyFrame {
 		int x = 30;
 		int y = 100;
 		
-		for(int i = 0; i < 3; i++) {
-		  while(x <=170) {
-		
-			//(1)の画面を消す
+		while(true) {
+			while(x<=170) {
 			clear();
-			//(2)四角形を表示する
 			setColor(0,128,0);
-			fillRect(x, y, 10, 100);
-			x += 5;
+			fillRect(x,y,10,100);
+			x +=5;
 			sleep(0.1);
-		  }
-		  while(x >= 30)
-			{   
-				//(1)の画面を消す
+			}
+			for(int i = 0; i<6; i++) {
 				clear();
-				//(2)四角形を表示する
 				setColor(0,128,0);
-				fillRect(x, y, 10, 100);
-				x -= 5;
+				fillRect(x,y,10,100);
+				y +=5;
 				sleep(0.1);
 			}
+			while(x>=30) {
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x -=5;
+				sleep(0.1);
+			}
+			for(int i = 0; i<6; i++) {
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				y +=5;
+				sleep(0.1);
+			}
+			
 		}
 		
+		
+	
 	}
 
 }
