889a3c443476b29b4528b075d9c0989e7475235c
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-27 10:50:54 +0900

    rennsyuu 5-1

--- src/Animation.java
+++ src/Animation.java
@@ -2,16 +2,30 @@
 public class Animation extends MyFrame {
 	public void run() {
 		int x = 30;
-		while(x <= 170)
-		{
+		int y = 100;
+		
+		while(y >= 50)
+		{   
 			//(1)の画面を消す
 			clear();
 			//(2)四角形を表示する
 			setColor(0,128,0);
-			fillOval(x, 80, 40, 40);
-			x += 5;
+			fillRect(x, y, 10, 100);
+			y -= 5;
 			sleep(0.1);
 		}
+		while(x <= 170)
+		{   
+			//(1)の画面を消す
+			clear();
+			//(2)四角形を表示する
+			setColor(0,128,0);
+			fillRect(x, y, 10, 100);
+			x += 5;
+			sleep(0.1);
+		
+	   }
+		
 	}
 
 }
