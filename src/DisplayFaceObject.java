53cad770a32402aff5d2982b56ccd5b15c6285ba
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 10:38:52 +0900

    6.19

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,18 +1,18 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1 =new Face(50,50);
+		Face face1 =new Face(50,50,10,5);
 		
-		Face face2= new Face(200,100);
+		Face face2= new Face(200,100,-10,-5);
 		for(int i=0; i<30; i++)
 		{
 			clear();
 			face1.draw(this);
 			face2.draw(this);
-			face1.x+= 10;
-			face1.y+= 5;
-			face2.x-= 10;
-			face2.y-= 5;
+			face1.x+= face1.vx;
+			face1.y+= face1.vy;
+			face2.x+= face2.vx;
+			face2.y+= face2.vy;
 			sleep(0.1);
 		}
 	}
--- src/Face.java
+++ src/Face.java
@@ -1,9 +1,11 @@
 
 public class Face {
-int x,y;
-public Face(int x, int y) {
+int x,y,vx,vy;
+public Face(int x, int y, int vx, int vy) {
 	this.x = x;
 	this.y = y;
+	this.vx = vx;
+	this.vy = vy;
 }
 public void draw(MyFrame frame) {
 	frame.fillOval(x,y,50,100);
