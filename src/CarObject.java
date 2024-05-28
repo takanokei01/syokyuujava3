6cd3096b5c1c27ec364d244818bf9aad01a72a35
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 11:36:43 +0900

    6.2a

--- src/CarObject.java
+++ src/CarObject.java
@@ -3,7 +3,7 @@ public class CarObject extends MyFrame{
 	public void run() {
 		Car car1 = new Car(50,50,5);
 		
-		Car car2 = new Car(200,100,-5);
+		Car car2 = new Car(200,200,-5);
 		for(int i= 0; i<30; i++) {
 			clear();
 			car1.draw(this);
