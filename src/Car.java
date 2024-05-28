c68a788134359c56b74a9fcf15b6eafd8b78e0fe
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 11:30:55 +0900

    rennsyuu6.2 movetukatta

--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,22 @@
 
 public class Car {
-int a,b;
+int a,b,c,d;
 
-public Car(int a,int b) {
+public Car(int a,int b,int c) {
 	this.a = a;
 	this.b = b;
+	this.c = c;
+	
 }
 public void draw(MyFrame frame) {
 	frame.fillOval(a+15,b+40,20,20);
 	frame.fillOval(a+50,b+40,20,20);
 	frame.fillRect(a+20,b,40,20);
 	frame.fillRect(a,b+20,80,20);
+	
+}
+public void move() {
+	a+= c;
 	
 }
 }
--- src/CarObject.java
+++ src/CarObject.java
@@ -1,15 +1,15 @@
 
 public class CarObject extends MyFrame{
 	public void run() {
-		Car car1 = new Car(50,50);
+		Car car1 = new Car(50,50,5);
 		
-		Car car2 = new Car(200,100);
+		Car car2 = new Car(200,100,-5);
 		for(int i= 0; i<30; i++) {
 			clear();
 			car1.draw(this);
 			car2.draw(this);
-			car1.a+= 5;
-			car2.a-= 5;
+			car1.move();
+			car2.move();
 			sleep(0.1);
 		}
 		
