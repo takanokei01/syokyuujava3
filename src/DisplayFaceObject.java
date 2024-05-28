bfe5dfd7981db81a81ab78e52b78c453cc3399d7
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 10:19:31 +0900

    6.14

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -2,13 +2,17 @@
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
 		Face face1 =new Face(50,50);
-		face1.draw(this);
 		
-		Face face2= new Face(250,50);
-		face2.draw(this);
-		
-		Face face3 = new Face(150,150);
-		face3.draw(this);
+		Face face2= new Face(200,100);
+		for(int i=0; i<30; i++)
+		{
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+			face1.x+= 10;
+			face2.x+= 10;
+			sleep(0.1);
+		}
 	}
 
 }
