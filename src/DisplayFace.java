d00ddcde9d62ccd43161a0bb0b365d8f3bd187e7
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 09:53:39 +0900

    6.3

--- src/DisplayFace.java
+++ src/DisplayFace.java
@@ -1,9 +1,10 @@
 
 public class DisplayFace extends MyFrame {
 	public void run() {
-		fillOval(130,40,50,100);
-		fillOval(230,40,50,100);
-		fillRect(130,200,150,10);
+		int x= 30;
+		fillOval(x,40,50,100);
+		fillOval(x+100,40,50,100);
+		fillRect(x,200,150,10);
 	}
 
 }
