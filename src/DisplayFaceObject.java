d30fd061aa492173d88438dcd4860720ee6e1b37
takanokei01 <zeal23410219@fsgcl.jp>
2024-05-28 10:13:43 +0900

    kakuninn 6.2

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -4,8 +4,11 @@ public class DisplayFaceObject extends MyFrame {
 		Face face1 =new Face(50,50);
 		face1.draw(this);
 		
-		Face face2= new Face(200,100);
+		Face face2= new Face(250,50);
 		face2.draw(this);
+		
+		Face face3 = new Face(150,150);
+		face3.draw(this);
 	}
 
 }
