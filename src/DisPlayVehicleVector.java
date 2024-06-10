934d15cd0786520c7be4bd52aac8504092a5c291
takanokei01 <zeal23410219@fsgcl.jp>
2024-06-10 11:39:57 +0900

    rensyuu8-1

--- src/DisPlayVehicleVector.java
+++ src/DisPlayVehicleVector.java
@@ -4,8 +4,11 @@ public class DisPlayVehicleVector extends MyFrame{
 	public void run() {
 		Vector<Vehicle> vehicles = new Vector<Vehicle>();
 		vehicles.add(new Car(10, 30, 3, 0));
-		vehicles.add(new Car(10, 100, 5, 0));
+		vehicles.add(new Car(50, 100, -5, 0));
 		vehicles.add(new Car(10, 150, 7, 0));
+		vehicles.add(new Car(50, 200,-5, 0));
+		vehicles.add(new Car(10, 260, 5, 0));
+		vehicles.add(new Car(10, 320, 6, 0));
 		
 		for(int i= 0; i<30; i++) {
 			clear();
--- src/DisplayCarsTrains.java
+++ src/DisplayCarsTrains.java
@@ -2,10 +2,13 @@
 public class DisplayCarsTrains extends MyFrame {
 
 	public void run() {
-		Vehicle[] vehicles = new Vehicle[3];
+		Vehicle[] vehicles = new Vehicle[6];
 		vehicles[0] = new Car(10, 30, 3, 0);
-		vehicles[1] = new Car(10, 100, 5, 0);
+		vehicles[1] = new Car(50, 100, -5, 0);
 		vehicles[2] = new Train(10, 150, 7, 0);
+		vehicles[3] = new Train(50, 200,-5, 0);
+		vehicles[4] = new Track(10, 260, 5, 0);
+		vehicles[5] = new Track(10, 320, 6, 0);
 		for (int i = 0; i<30; i++) {
 			clear();
 			for(int j= 0; j<vehicles.length; j++) {
